407db1ff3bb490870f322f8d856cf3a58c9b6464
SiddheshGajare <ishugajare90468@gmail.com>
2025-02-06 23:58:42 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import StockPrediction from "./components/StockPrediction.tsx";
 import SignUpForm from '../src/SignUpForm';
 import SignInPage from '../src/SignIn';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="p-6">
-      <SignUpForm />
+      <StockPrediction/>
     </div>
   );
 }
