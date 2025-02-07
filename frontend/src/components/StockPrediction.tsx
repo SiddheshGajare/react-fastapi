e104b526b62e8a3b8f783db4df1cacd87cedb865
SiddheshGajare <ishugajare90468@gmail.com>
2025-02-07 13:40:53 +0530

    indian stocks

--- frontend/src/components/StockPrediction.tsx
+++ frontend/src/components/StockPrediction.tsx
@@ -52,6 +52,11 @@ function StockDashboard () {
     value: value, // Predicted stock price
   }));
 
+  const handleBlur = () => {
+    if (ticker && !ticker.endsWith(".NS")) {
+      setTicker(ticker + ".NS"); // Save with ".NS" on blur
+    }
+  };
   return (
     <div className="dashboard-container">
       <div className="dashboard-layout">
@@ -127,12 +132,15 @@ function StockDashboard () {
             <div className='search'>
             <input
               type="text"
-              value={ticker}
+              value={ticker.replace(".NS", "")} 
               onChange={(e) => setTicker(e.target.value)}
+              onBlur={handleBlur}
               placeholder="Search for various stocks........"
               className="search-input"
             />
-            <button onClick={fetchPredictions} className="search-button">🔍</button>
+            <button onClick={fetchPredictions} className="search-button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+</svg></button>
             </div>
             <div className="header-actions">
               <Mail />
